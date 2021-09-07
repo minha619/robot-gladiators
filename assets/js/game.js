47dea80bf4e0414159545eb5cab411f7715cfe7b
Your Name <minhakim@mins-mbp.home>
2021-09-06 23:11:26 -0400

    player name prompt error fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,8 +160,19 @@ var shop = function () {
     }
 };
 
+//function to set name
+var getPlayerName = function(){
+    var name="";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attach: 10,
     money: 10,
